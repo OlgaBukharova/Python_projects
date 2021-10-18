c7f4955acc8f587f33d8e1765132b01a84575a69
OlgaBukharova <91290171+OlgaBukharova@users.noreply.github.com>
2021-10-18 03:46:29 +0300

    Delete turtle_task_1 directory

--- turtle_task_1/turtle_4.py
+++
@@ -1,7 +0,0 @@
-import turtle
-
-turtle.shape ('turtle')
-
-for x in range(360):
-    turtle.forward (1)
-    turtle.left(1)
--- turtle_task_1/turtle_7.py
+++
@@ -1,9 +0,0 @@
-import turtle
-from math import pi, sin, cos
-turtle.shape ('turtle')
-
-for i in range (400):
-    t=i*pi/10
-    x=t*cos(t)
-    y=t*sin(t)
-    turtle.goto(x,y)
